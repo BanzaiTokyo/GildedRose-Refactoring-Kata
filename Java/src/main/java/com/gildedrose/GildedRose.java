0841248392377baacb0537c37e65329465143776
BanzaiTokyo <banzaitokyo@gmail.com>
2025-01-05 22:51:15 +0100

    simplify ifs

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,7 +2,7 @@ package com.gildedrose;
 
 class GildedRose {
     public static final String AGED_BRIE = "Aged Brie";
-    public static final String PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
@@ -17,54 +17,43 @@ class GildedRose {
     }
 
     private static void updateItem(Item item) {
-        if (!item.name.equals(AGED_BRIE)
-            && !item.name.equals(PASSES)) {
-            if (item.quality > 0) {
-                if (!item.name.equals(SULFURAS)) {
-                    decreaseQuality(item);
-                }
-            }
-        } else {
+        if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE)) {
             if (item.quality < 50) {
                 increaseQuality(item);
 
-                if (item.name.equals(PASSES)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            increaseQuality(item);
-                        }
+                if (item.name.equals(BACKSTAGE)) {
+                    if (item.sellIn <= 10 && item.quality < 50) {
+                        increaseQuality(item);
                     }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            increaseQuality(item);
-                        }
+                    if (item.sellIn <= 5 && item.quality < 50) {
+                        increaseQuality(item);
                     }
                 }
             }
+        } else if (!item.name.equals(SULFURAS) && item.quality > 0) {
+            decreaseQuality(item);
+        }
+        if (!item.name.equals(SULFURAS)) {
+            decreaseSellIn(item);
         }
 
-        if (!item.name.equals(SULFURAS)) {
-            item.sellIn = item.sellIn - 1;
-        }
 
         if (item.sellIn < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(PASSES)) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(SULFURAS)) {
-                            decreaseQuality(item);
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
+            if (item.name.equals(AGED_BRIE)) {
                 if (item.quality < 50) {
                     increaseQuality(item);
                 }
+            } else if (item.name.equals(BACKSTAGE)) {
+                item.quality = 0;
+            } else if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+                decreaseQuality(item);
             }
         }
+    }
+
+    private static void decreaseSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
     }
 
     private static void decreaseQuality(Item item) {
