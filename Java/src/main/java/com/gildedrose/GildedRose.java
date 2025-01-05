b71d9033b17df767afcc0002a6c359c0671e8a36
BanzaiTokyo <banzaitokyo@gmail.com>
2025-01-06 00:25:10 +0100

    Move methods to InventoryItem

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,8 +1,6 @@
 package com.gildedrose;
 
-import com.gildedrose.inventory.AgedBrie;
-import com.gildedrose.inventory.BackstagePass;
-import com.gildedrose.inventory.Sulfuras;
+import com.gildedrose.inventory.InventoryItem;
 
 class GildedRose {
     Item[] items;
@@ -13,62 +11,10 @@ class GildedRose {
 
     public void dailyUpdate() {
         for (Item item : items) {
-            updateItem(item);
+            InventoryItem.create(item).updateItem();
         }
     }
 
-    private static void updateItem(Item item) {
-        if (!item.name.equals(Sulfuras.NAME)) {
-            decreaseSellIn(item);
-        }
-        updateQuality(item);
-    }
 
 
-    private static void updateQuality(Item item) {
-        if (item.name.equals(AgedBrie.NAME) || item.name.equals(BackstagePass.NAME)) {
-            if (item.quality < 50) {
-                increaseQuality(item);
-
-                if (item.name.equals(BackstagePass.NAME)) {
-                    if (item.sellIn < 10 && item.quality < 50) {
-                        increaseQuality(item);
-                    }
-
-                    if (item.sellIn < 5 && item.quality < 50) {
-                        increaseQuality(item);
-                    }
-                }
-            }
-        } else if (!item.name.equals(Sulfuras.NAME) && item.quality > 0) {
-            decreaseQuality(item);
-        }
-
-
-
-        if (item.sellIn < 0) {
-            if (item.name.equals(AgedBrie.NAME)) {
-                if (item.quality < 50) {
-                    increaseQuality(item);
-                }
-            } else if (item.name.equals(BackstagePass.NAME)) {
-                item.quality = 0;
-            } else if (item.quality > 0 && !item.name.equals(Sulfuras.NAME)) {
-                decreaseQuality(item);
-            }
-        }
-
-    }
-
-    private static void decreaseSellIn(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private static void decreaseQuality(Item item) {
-        item.quality -= 1;
-    }
-
-    private static void increaseQuality(Item item) {
-        item.quality += 1;
-    }
 }
--- Java/src/main/java/com/gildedrose/inventory/InventoryItem.java
+++ Java/src/main/java/com/gildedrose/inventory/InventoryItem.java
@@ -17,4 +17,59 @@ public class InventoryItem {
                 return new StandardItem(item);
         }
     }
+
+    public void updateItem() {
+        if (!this.item.name.equals(Sulfuras.NAME)) {
+            decreaseSellIn();
+        }
+        updateQuality();
+        updateExpired();
+    }
+
+    private void updateExpired() {
+        if (item.sellIn < 0) {
+            if (item.name.equals(AgedBrie.NAME)) {
+                if (item.quality < 50) {
+                    increaseQuality();
+                }
+            } else if (item.name.equals(BackstagePass.NAME)) {
+                item.quality = 0;
+            } else if (item.quality > 0 && !item.name.equals(Sulfuras.NAME)) {
+                decreaseQuality();
+            }
+        }
+    }
+
+    private void updateQuality() {
+        if (item.name.equals(AgedBrie.NAME) || item.name.equals(BackstagePass.NAME)) {
+            if (item.quality < 50) {
+                increaseQuality();
+
+                if (item.name.equals(BackstagePass.NAME)) {
+                    if (item.sellIn < 10 && item.quality < 50) {
+                        increaseQuality();
+                    }
+
+                    if (item.sellIn < 5 && item.quality < 50) {
+                        increaseQuality();
+                    }
+                }
+            }
+        } else if (!item.name.equals(Sulfuras.NAME) && item.quality > 0) {
+            decreaseQuality();
+        }
+    }
+
+    private  void decreaseSellIn() {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private  void decreaseQuality() {
+        item.quality -= 1;
+    }
+
+    private  void increaseQuality() {
+        item.quality += 1;
+    }
+
 }
