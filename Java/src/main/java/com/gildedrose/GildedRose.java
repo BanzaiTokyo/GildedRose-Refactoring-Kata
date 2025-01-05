be0aca718f9ce33ea595ca2ea2c268b8c0864238
BanzaiTokyo <banzaitokyo@gmail.com>
2025-01-05 22:51:15 +0100

    simplify ifs

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,7 +2,7 @@ package com.gildedrose;
 
 class GildedRose {
     public static final String AGED_BRIE = "Aged Brie";
-    public static final String PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
@@ -17,29 +17,28 @@ class GildedRose {
     }
 
     private static void updateItem(Item item) {
-        if (!item.name.equals(AGED_BRIE)
-            && !item.name.equals(PASSES)) {
-            if (item.quality > 0) {
-                if (!item.name.equals(SULFURAS)) {
-                    decreaseQuality(item);
-                }
-            }
-        } else {
+        if (item.name.equals(AGED_BRIE) || item.name.equals(BACKSTAGE)) {
             if (item.quality < 50) {
                 increaseQuality(item);
 
-                if (item.name.equals(PASSES)) {
-                    if (item.sellIn < 11) {
+                if (item.name.equals(BACKSTAGE)) {
+                    if (item.sellIn <= 10) {
                         if (item.quality < 50) {
                             increaseQuality(item);
                         }
                     }
 
-                    if (item.sellIn < 6) {
+                    if (item.sellIn <= 5) {
                         if (item.quality < 50) {
                             increaseQuality(item);
                         }
                     }
+                }
+            }
+        } else {
+            if (item.quality > 0) {
+                if (!item.name.equals(SULFURAS)) {
+                    decreaseQuality(item);
                 }
             }
         }
@@ -49,20 +48,14 @@ class GildedRose {
         }
 
         if (item.sellIn < 0) {
-            if (!item.name.equals(AGED_BRIE)) {
-                if (!item.name.equals(PASSES)) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(SULFURAS)) {
-                            decreaseQuality(item);
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
+            if (item.name.equals(AGED_BRIE)) {
                 if (item.quality < 50) {
                     increaseQuality(item);
                 }
+            } else if (item.name.equals(BACKSTAGE)) {
+                item.quality = 0;
+            } else if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+                decreaseQuality(item);
             }
         }
     }
